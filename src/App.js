2775b266de629e136b383cab4ad49b2bcc1c0d9e
Sekou Seh <sehsekou98@gmail.com>
2022-06-10 03:48:11 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Skills from "./components/Skills"
+import Work from "./components/Work";
+
+
+function App() {
+  return (
+    <div>
+   <Navbar/>
+   <Home />
+   <About />
+   <Skills />
+   <Work />
+   <Contact />
+   
+    </div> 
+      
+   ); 
+}
+
+export default App;
